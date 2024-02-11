da05d45b6057b83d7959bce4f2ef40a5d90a12f4
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2024-02-11 17:53:18 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # 必要なフォルダを作成
+mkdir -p bin
+mkdir -p asm
 mkdir -p out
-mkdir -p asmmbly
 
 # srcフォルダ内の全ての.cファイルに対してループ
 for file in src/*.c; do
@@ -10,7 +11,7 @@ for file in src/*.c; do
   base_name=$(basename "$file" .c)
 
   # .cファイルをコンパイルして.exeをoutフォルダに出力
-  gcc -o "out/${base_name}.exe" "$file"
+  gcc -o "bin/${base_name}.exe" "$file"
 
   # アセンブリコードを生成してasmフォルダに出力
   gcc -S -o "asm/${base_name}.s" "$file"
